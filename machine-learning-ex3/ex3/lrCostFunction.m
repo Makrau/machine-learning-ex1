ea8d8c7abfd366911c2757f85069c73a2a99d074
Makrau <iago006@hotmail.com>
2018-09-16 00:16:28 -0300

    realizado o exercicio de calcular a regressão logistica regularizada utilizando vetorização

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -36,17 +36,26 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
-
-
-
-
-
-
-
-
-
 % =============================================================
+fprintf('\nTamanho X: %dx%d\n', size(X));
+fprintf('Tamanho y: %dx%d\n', size(y));
+fprintf('Tamanho theta: %dx%d\n', size(theta));
+fprintf('Tamanho theta transposto * X transposto: %dx%d\n', size(theta' * X'));
+% função custo sem regularização
+J = -(1 / m) .* sum(y' * log(sigmoid(X * theta)) + (1-y') * log(1 - sigmoid(X * theta)));
 
-grad = grad(:);
+temp = theta;
+temp(1) = 0;
+% regularização da função de custo
+ J = J + lambda/(2*m) * sum(temp.^2);
+
+% Gradient sem regularização
+grad = (1 / m) .* X' * (sigmoid(X * theta) - y);
+
+% regularização do gradient
+grad = grad + lambda/m .*temp;
+
+fprintf('Tamanho J: %dx%d\n', size(J));
+fprintf('Tamanho grad: %dx%d\n', size(grad));
 
 end
