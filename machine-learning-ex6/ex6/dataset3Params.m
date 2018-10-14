a883158fbacacb14821bf1e0f24193bfc9b69a48
Makrau <iago006@hotmail.com>
2018-10-14 18:51:37 -0300

    resolvida questao de otimizacao dos parametros C e sigma

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -22,6 +22,38 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+x1 = [1 2 1]; x2 = [0 4 -1]; 
+test_values = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
+C = test_values(1);
+sigma = test_values(1);
+
+% the first prediction error for reference to compare
+model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma)); 
+predictions = svmPredict(model, Xval);
+minPredictionError = mean(double(predictions ~= yval));
+
+number_of_test_values = size(test_values, 1);
+for indexC = 1 : number_of_test_values
+	loopC = test_values(indexC);
+	for indexSigma = 1 : number_of_test_values
+		loopSigma = test_values(indexSigma);
+		fprintf('training with loopC = %f and loopSigma = %f\n', loopC, loopSigma);
+		model= svmTrain(X, y, loopC, @(x1, x2) gaussianKernel(x1, x2, loopSigma)); 
+		predictions = svmPredict(model, Xval);
+		loopPredictionError = mean(double(predictions ~= yval));
+		if (loopPredictionError < minPredictionError)
+			fprintf('new minimal error: %f\n', loopPredictionError);
+			minPredictionError = loopPredictionError;
+			C = loopC;
+			sigma = loopSigma;
+			fprintf('sigma = %f\nC = %f\n', sigma, C);
+		end
+	end
+end
+
+fprintf('Final sigma = %f\nFinal C = %f\n', sigma, C);
+
+[C, sigma];
 
 
 
